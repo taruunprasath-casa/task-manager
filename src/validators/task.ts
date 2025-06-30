822d5cddd244cb54bcc1304260ce72c534654c56
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-06-30 16:31:43 +0530

    Taruun | Added elements related to Task Table

--- src/validators/task.ts
+++ src/validators/task.ts
@@ -1,10 +1,13 @@
-import {z} from "zod"
+import { z } from "zod";
 
-const name =z.string().min(1,{message:"Field can't be empty"})
+const name = z.string().min(1, { message: "Task Name can't be empty" });
+const description = z.string().min(1, { message: "Description can't be empty" });
 
 const taskData = z.object({
-    taskname:name
+  name: name,
+  description: description,
+  estimateDate: z.date().optional(),
+  repo_id:z.number().optional()
 });
 
-export default {taskData};
-
+export default { taskData };
