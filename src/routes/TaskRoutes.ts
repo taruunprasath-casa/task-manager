c8832e6480a8880ea4fcc827ca7f298db75db81a
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-06-30 14:48:19 +0530

    Taruun | Created TaskRoutes

---
+++ src/routes/TaskRoutes.ts
@@ -0,0 +1,12 @@
+import  express  from "express";
+import TaskController from "../controllers/TaskController";
+const router = express.Router();
+
+router.post('/', TaskController.createTask);
+router.get('/', TaskController.getAllTask);
+router.get('/:id', TaskController.getTaskById);
+router.put('/:id', TaskController.updateTask);
+router.delete('/:id',TaskController.deleteTask);
+
+
+export default router;
