8ac78587d5a9edbb1749ebf70d2514087e2374ff
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-02 13:15:53 +0530

    Taruun | Added route call for tasks

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 
 import express from "express";
 import userRouter from "./routes/UserRoutes"; 
+import taskRouter from "./routes/TaskRoutes";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", userRouter); 
+app.use("/tasks",taskRouter);
 
 app.listen(3000, () => {
   console.log("Server running on port 3000");
