bf8de95df3c9c6c9daec69c56fc47930fd2f3477
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-02 13:18:07 +0530

    Taruun | renaming and bug fixes

--- src/validators/task.ts
+++ src/validators/task.ts
@@ -1,20 +1,27 @@
 import { z } from "zod";
 
 const name = z.string().min(1, { message: "Task Name can't be empty" });
-const description = z.string().min(1, { message: "Description can't be empty" });
+const description = z
+  .string()
+  .min(1, { message: "Description can't be empty" });
 
 const taskData = z.object({
   name: name,
   description: description,
-  estimatedDate: z.date().optional(),
-  repos:z.object({
-    repoId:z.number(),
-    branchName: z.string().optional(),
-  }).optional(),
-  users:z.object({
-    userId:z.number(),
-    roleId:z.number(),
-  })
+  estimatedDate: z.coerce.date().optional(),
+  repos: z
+    .object({
+      repoId: z.number(),
+      branchName: z.string(),
+    })
+    .array()
+    .optional(),
+  users: z
+    .object({
+      userId: z.number(),
+      roleId: z.number(),
+    })
+    .array(),
 });
 
 export default { taskData };
