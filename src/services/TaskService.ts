90f4a09a64d562167deeeb87b3fd6eb32b5cc9ba
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-03 14:54:25 +0530

    Taruun | Added filter condition to the function getAllTask

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,23 +1,64 @@
+import { sequelize } from "../db/sequelize";
 import { TaskData } from "../interfaces/task";
+import { Role } from "../models/Role";
+import { Stages } from "../models/Stages";
 import { Task } from "../models/Task";
+import { User } from "../models/User";
 import { UserTask } from "../models/UserTask";
 
 class TaskService {
-  async createTask(task: TaskData){
-    return await Task.create(
-      {
-        name: task.name,
-        description: task.description,
-        estimatedDate: task.estimatedDate,
-        
-      },
-    );
+  async createTask(task: TaskData) {
+    return await Task.create({
+      name: task.name,
+      description: task.description,
+      estimatedDate: task.estimatedDate,
+      stage_id: task.stageId,
+    });
   }
   async getAllTask() {
-    return await Task.findAll();
+    return await Task.findAll({
+      order: [["stage_id", "DESC"]],
+      include: [
+        {
+          model: UserTask,
+          as: "userTasks",
+          include: [
+            {
+              model: User,
+              attributes: ["name"],
+              as: "user",
+            }
+          ]
+        }
+      ]
+    });
   }
   async getTaskById(id: string): Promise<any> {
-    return await Task.findByPk(id);
+    return await Task.findByPk(id, {
+      include: [
+        {
+          model: UserTask,
+          as: "userTasks",
+          attributes: ["task_id", "user_id", "role_id"],
+          include: [
+            {
+              model: User,
+              attributes: ["id", "name"],
+              as: "user",
+            },
+            {
+              model: Role,
+              attributes: ["id", "name"],
+              as: "role",
+            },
+          ],
+        },
+        {
+          model: Stages,
+          attributes: ["id", "name"],
+        },
+      ],
+    });
   }
   async updateTask(id: string, data: TaskData) {
     const task = await Task.findByPk(id);
