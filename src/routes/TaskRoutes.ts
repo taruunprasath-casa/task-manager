d32ecdc9e3701409b55b30e3d32592c483cc7b01
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-04 10:46:59 +0530

    Taruun | changed the get api to post api

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -3,7 +3,7 @@ import TaskController from "../controllers/TaskController";
 const router = express.Router();
 
 router.post('/', TaskController.createTask);
-router.get('/', TaskController.getAllTask);
+router.post('/list', TaskController.getAllTask);
 router.get('/:id', TaskController.getTaskById);
 router.put('/:id', TaskController.updateTask);
 router.delete('/:id',TaskController.deleteTask);
