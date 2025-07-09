6fcf77b5cf4dd44e16c5cda29eccb3743961129a
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-09 12:32:36 +0530

    Taruun | Created interface for update data and repo data

--- src/interfaces/task.ts
+++ src/interfaces/task.ts
@@ -1,5 +1,16 @@
 import { z } from "zod";
 import task from "../validators/task";
 
-export type TaskData = z.infer<typeof task.taskData>
-export type TaskFilters = z.infer<typeof task.taskFilters>
+export type TaskData = z.infer<typeof task.taskData>;
+export type TaskFilters = z.infer<typeof task.taskFilters>;
+export interface TaskUpdateData {
+  id: number;
+  name: string;
+  description: string;
+  stage_id: number;
+}
+export interface TaskRepoData {
+  task_id: number;
+  repo_id: number;
+  task_branch_name: string;
+}
