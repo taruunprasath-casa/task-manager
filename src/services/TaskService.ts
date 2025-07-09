a1f986c7e7d794bae39ce00fc5fe97d57ca49012
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-09 12:32:00 +0530

    Taruun | added transaction control to update service

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -1,10 +1,12 @@
-import { Op } from "sequelize";
-import { TaskData, TaskFilters } from "../interfaces/task";
+import { Op, Transaction } from "sequelize";
+import { TaskData, TaskFilters, TaskUpdateData } from "../interfaces/task";
 import { Role } from "../models/Role";
 import { Stages } from "../models/Stages";
 import { Task } from "../models/Task";
 import { User } from "../models/User";
 import { UserTask } from "../models/UserTask";
+import { sequelize } from "../db/sequelize";
+import TaskRepoService from "./TaskRepoService";
 
 class TaskService {
   async createTask(task: TaskData) {
@@ -15,7 +17,7 @@ class TaskService {
       stage_id: task.stageId,
     });
   }
-  async getAllTask(taskFilters:TaskFilters) {
+  async getAllTask(taskFilters: TaskFilters) {
     return await Task.findAll({
       order: [["stage_id", "DESC"]],
       include: [
@@ -61,14 +63,41 @@ class TaskService {
       ],
     });
   }
-  async updateTask(id: string, data: TaskData) {
-    return await Task.update(data, {
-      where: { id: id },
-      returning: true,
-      });
-  }
+  async updateTaskTransactionService  (
+  taskData: TaskData,
+  taskId: number
+) {
+  await sequelize.transaction(async (t) => {
+    const updatedTask: TaskUpdateData = {
+      id: taskId,
+      name: taskData.name,
+      description: taskData.description,
+      stage_id: taskData.stageId,
+    };
+    await this.updateTask(t, updatedTask, taskId);
+    const mappedTaskRepo = taskData.repos?.map((repo) => ({
+      repo_id: Number(repo.repoId),
+      task_branch_name: String(repo.branchName),
+      task_id: Number(taskId),
+    })) || [];
+    await TaskRepoService.handleUpdateTaskRepos(t, mappedTaskRepo, taskId);
+  });
+};
+
+async updateTask (
+  t: Transaction,
+  taskData: TaskUpdateData,
+  taskId: number
+){
+  return await Task.update(taskData, {
+    where: {
+      id: taskId,
+    },
+    transaction: t,
+  });
+};
   async deleteTask(id: string) {
-    const task = await Task.findByPk(id);
+    const task = await Task.findByPk(Number(id));
     if (!task) throw new Error("Task Not Found");
     await task.destroy();
     return true;
