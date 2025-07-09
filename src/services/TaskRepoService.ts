9d2fa37c148ca3894bf5b85ceca0278a09b7fbf8
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-09 12:34:19 +0530

    Taruun | updated the functionalites and included transaction control to the services

--- src/services/TaskRepoService.ts
+++ src/services/TaskRepoService.ts
@@ -1,14 +1,45 @@
+import { Transaction } from "sequelize";
 import { TaskRepo } from "../models/TaskRepo";
+import { TaskRepoData } from "../interfaces/task";
 
-class TaskRepoService{
-    async createTaskRepo(taskService:{
-        task_id:number;
-        repo_id:number;
-        task_branch_name:string;
-        
-    }[]){
-        return await TaskRepo.bulkCreate(taskService); 
-    }
+class TaskRepoService {
+  async createTaskRepo(
+    taskRepoData: TaskRepoData[],
+    transaction?: Transaction
+  ) {
+    return await TaskRepo.bulkCreate(taskRepoData, {
+      updateOnDuplicate: ["repo_id", "task_branch_name", "updatedAt"],
+      transaction,
+    });
+  }
+
+  async deleteTaskRepo(transaction: Transaction, taskRepoData: TaskRepo[]) {
+    return taskRepoData.map(async (taskRepo) => {
+       await TaskRepo.destroy({
+        where: { task_id: taskRepo.task_id, repo_id: taskRepo.repo_id },
+        transaction,
+      });
+    });
+  }
+  async handleUpdateTaskRepos(
+  t: Transaction,
+  taskRepos: TaskRepoData[],
+  taskId: number
+) {
+  this.createTaskRepo(taskRepos, t);
+  const taskRepoExistingData = await TaskRepo.findAll({
+    where: { task_id: taskId },
+  });
+  const removedTaskRepos = taskRepoExistingData.filter((existingData) =>
+    !taskRepos.some(
+      (data) =>
+        data.repo_id == existingData.repo_id &&
+        data.task_id == existingData.task_id
+    )
+  );
+  await this.deleteTaskRepo(t,removedTaskRepos);
+};
 }
+
 
 export default new TaskRepoService();
--- src/services/UserTaskService.ts
+++ src/services/UserTaskService.ts
@@ -1,31 +1,87 @@
-
+import { Transaction } from "sequelize";
 import { UserTask } from "../models/UserTask";
+import { UserTaskData } from "../interfaces/user";
+import { sequelize } from "../db/sequelize";
 
-class UserTaskService{
-    async createUserTasks(userTasks: {
-        task_id:number,
-        user_id:number,
-        role_id:number,
-    }[] ){
-        return await UserTask.bulkCreate(userTasks);
-    }
-    async getAllUserTask(){
-        return await UserTask.findAll();
-    }
-    async getUserTaskById(id: string){
-        return await UserTask.findByPk(id);
-    }
-    async updateUserTask(id:string,updatedData:UserTask){
-        const userTask = await UserTask.findByPk(id);
-        if(!userTask) throw new Error("User Task Not Found");
-        return await userTask.update(updatedData);
-    }
-    async deleteUserTask(id:string){
-        const userTask = await UserTask.findByPk(id);
-        if(!userTask) throw new Error("User Task Not found");
-        await userTask.destroy();
-        return true;
-    }
+class UserTaskService {
+  async createUserTasks(
+    userTasks: {
+      task_id: number;
+      user_id: number;
+      role_id: number;
+    }[]
+  ) {
+    return await UserTask.bulkCreate(userTasks);
+  }
+  async getAllUserTask() {
+    return await UserTask.findAll();
+  }
+  async getUserTaskById(id: string) {
+    return await UserTask.findByPk(id);
+  }
+  async deleteUserTask(transaction: Transaction, userTaskData: UserTaskData[]) {
+    return userTaskData.map(async (userTask) => {
+      await UserTask.destroy({
+        where: {
+          task_id: userTask.task_id,
+          user_id: userTask.user_id,
+          role_id: userTask.role_id,
+        },
+        transaction,
+      });
+    });
+  }
+
+  async updateUserTaskService(userTaskData: UserTaskData[], userId: number) {
+    const mappedUserTask = userTaskData.map((userTask) => ({
+      task_id: Number(userTask.task_id),
+      user_id: Number(userTask.user_id),
+      role_id: Number(userTask.role_id),
+    }));
+
+    await sequelize.transaction(async (t) => {
+      for (const userTask of mappedUserTask) {
+        await this.updateUserTask(t, userTask, userTask.user_id);
+      }
+
+      await this.createUserTasks(mappedUserTask);
+
+      const userTaskExistingData = await UserTask.findAll({
+        where: { user_id: userId },
+        transaction: t,
+      });
+
+      const removeUserTasks = userTaskExistingData.filter(
+        (existingData) =>
+          !mappedUserTask.some(
+            (data) =>
+              data.user_id === existingData.user_id &&
+              data.task_id === existingData.task_id
+          )
+      );
+      await this.deleteUserTask(
+        t,
+        removeUserTasks.map((task) => ({
+          task_id: task.task_id as number,
+          user_id: task.user_id as number,
+          role_id: task.role_id as number,
+        }))
+      );
+    });
+  }
+
+  async updateUserTask(
+    t: Transaction,
+    userTaskData: UserTaskData,
+    userId: number
+  ) {
+    return await UserTask.update(userTaskData, {
+      where: {
+        user_id: userId,
+      },
+      transaction: t,
+    });
+  }
 }
 
 export default new UserTaskService();
