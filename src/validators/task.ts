ee82fe226d23fef0250aff6233ab103d90cac49e
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-09 12:30:27 +0530

    Taruun | Extract the taskRepo seperately

--- src/validators/task.ts
+++ src/validators/task.ts
@@ -4,17 +4,17 @@ const name = z.string().min(1, { message: "Task Name can't be empty" });
 const description = z
   .string()
   .min(1, { message: "Description can't be empty" });
-
+const taskRepo = z
+    .object({
+      repoId: z.number(),
+      branchName: z.string(),
+    })
 const taskData = z.object({
   name: name,
   description: description,
   estimatedDate: z.coerce.date().optional(),
   stageId:z.number(),
-  repos: z
-    .object({
-      repoId: z.number(),
-      branchName: z.string(),
-    })
+  repos: taskRepo
     .array()
     .optional(),
   users: z
@@ -29,4 +29,4 @@ const taskFilters = z.object({
   userIds: z.number().array()
 })
 
-export default { taskData, taskFilters };
+export default { taskData, taskFilters, taskRepo};
