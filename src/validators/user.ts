2fdd95993b4acea8dfb13d8c31689a5b6a4b487d
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-09 12:29:59 +0530

    Taruun | Added userTaskData object

--- src/validators/user.ts
+++ src/validators/user.ts
@@ -3,5 +3,10 @@ import { z } from "zod";
 const userData = z.object({
   name:z.string().min(1,{message:"User Name cannot be empty"})  
 });
+const userTaskData = z.object({
+  task_id: z.number(),
+  user_id: z.number(),
+  role_id: z.number(),
+})
 
-export default {userData}
+export default {userData,userTaskData}
