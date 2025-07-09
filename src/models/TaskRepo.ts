584f85586defbd859ef817b23f8f9f7bafd2bd75
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-09 12:33:31 +0530

    Taruun | Included task_id and repo_id in the init

--- src/models/TaskRepo.ts
+++ src/models/TaskRepo.ts
@@ -1,44 +1,64 @@
-import { CreationOptional, DataTypes, ForeignKey, InferAttributes, Model } from "sequelize";
-import { sequelize } from "../db/sequelize";
-import { Task } from "./Task";
-import { Repo } from "./Repo";
+    import { CreationOptional, DataTypes, ForeignKey, InferAttributes, Model } from "sequelize";
+    import { sequelize } from "../db/sequelize";
+    import { Task } from "./Task";
+    import { Repo } from "./Repo";
 
-export class TaskRepo extends Model<InferAttributes<TaskRepo>>{
-    declare id?: CreationOptional<number>;
-    declare task_id: ForeignKey<Task["id"]>;
-    declare repo_id: ForeignKey<Repo["id"]>;
-    declare task_branch_name: String;
-    declare createdAt?:CreationOptional<Date>;
-    declare updatedAt?:CreationOptional<Date>;
-    
-}
-
-TaskRepo.init({
-    id:{
-        type:DataTypes.NUMBER,
-        primaryKey:true,
-        autoIncrement:true,
-    },
-    task_branch_name:{
-        type:DataTypes.STRING,
-        allowNull:false,
-    },
-    createdAt: {
-      type: DataTypes.DATE(3),
-      defaultValue: sequelize.literal("current_timestamp(3)"),
-      allowNull: false,
-    },
-    updatedAt: {
-      type: DataTypes.DATE(3),
-      defaultValue: sequelize.literal("current_timestamp(3)"),
-      allowNull: false,
-    },
-},
-    {
-        sequelize,
-        tableName:"task_repo",
-        modelName:"taskrepo",
-        timestamps:true,
+    export class TaskRepo extends Model<InferAttributes<TaskRepo>>{
+        declare id?: CreationOptional<number>;
+        declare task_id: ForeignKey<Task["id"]>;
+        declare repo_id: ForeignKey<Repo["id"]>;
+        declare task_branch_name: String;
+        declare createdAt?:CreationOptional<Date>;
+        declare updatedAt?:CreationOptional<Date>;
+        
     }
-);
+
+    TaskRepo.init({
+        id:{
+            type:DataTypes.NUMBER,
+            primaryKey:true,
+            autoIncrement:true,
+        },
+        task_branch_name:{
+            type:DataTypes.STRING,
+            allowNull:false,
+        },
+        task_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Task,
+                key: 'id',
+            },
+        },
+        repo_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            references: {
+                model: Repo,
+                key: 'id',
+            },
+        },
+        createdAt: {
+        type: DataTypes.DATE(3),
+        defaultValue: sequelize.literal("current_timestamp(3)"),
+        allowNull: false,
+        },
+        updatedAt: {
+        type: DataTypes.DATE(3),
+        defaultValue: sequelize.literal("current_timestamp(3)"),
+        allowNull: false,
+        },
+        
+    },
+    
+        {
+            sequelize,
+            tableName:"task_repo",
+            modelName:"taskrepo",
+            timestamps:true,
+            
+        }
+        
+    );
 
