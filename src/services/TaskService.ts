da87da5e1da01624c6ab0a311d6ad33d2b3d9203
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-10 17:27:30 +0530

    Taruun | Added transaction and custom filters

--- src/services/TaskService.ts
+++ src/services/TaskService.ts
@@ -7,24 +7,52 @@ import { User } from "../models/User";
 import { UserTask } from "../models/UserTask";
 import { sequelize } from "../db/sequelize";
 import TaskRepoService from "./TaskRepoService";
+import { userTaskData } from "../interfaces/user";
+import UserTaskService from "./UserTaskService";
 
 class TaskService {
-  async createTask(task: TaskData) {
-    return await Task.create({
-      name: task.name,
-      description: task.description,
-      estimatedDate: task.estimatedDate,
-      stage_id: task.stageId,
-    });
+  async createTask(task: TaskData, transaction?: Transaction) {
+    return await Task.create(
+      {
+        name: task.name,
+        description: task.description,
+        estimatedDate: task.estimatedDate,
+        stage_id: task.stageId,
+      },
+      {
+        logging: true,
+        returning: true,
+        transaction,
+      }
+    );
   }
   async getAllTask(taskFilters: TaskFilters) {
+    const userIds = taskFilters?.userIds;
+    const fromEstimatedDate = taskFilters?.fromEstimatedDate;
+    const toEstimatedDate = taskFilters?.toEstimatedDate;
+
+    const userTaskWhere: any = {};
+    const estimatedDateWhere: any = {};
+
+    if (userIds && userIds.length > 0) {
+      userTaskWhere.user_id = {
+        [Op.in]: userIds,
+      };
+    }
+
+    if (fromEstimatedDate && toEstimatedDate) {
+      estimatedDateWhere.estimated_date = {
+        [Op.between]: [fromEstimatedDate, toEstimatedDate],
+      };
+    }
+
     return await Task.findAll({
-      order: [["stage_id", "DESC"]],
       include: [
         {
           model: UserTask,
+          attributes: ["createdAt", "updatedAt"],
           as: "userTasks",
-          where: { user_id: { [Op.in]: taskFilters } },
+          where: userTaskWhere,
           include: [
             {
               model: User,
@@ -34,8 +62,10 @@ class TaskService {
           ],
         },
       ],
+      where: estimatedDateWhere,
     });
   }
+
   async getTaskById(id: string): Promise<any> {
     return await Task.findByPk(id, {
       include: [
@@ -63,39 +93,39 @@ class TaskService {
       ],
     });
   }
-  async updateTaskTransactionService  (
-  taskData: TaskData,
-  taskId: number
-) {
-  await sequelize.transaction(async (t) => {
-    const updatedTask: TaskUpdateData = {
-      id: taskId,
-      name: taskData.name,
-      description: taskData.description,
-      stage_id: taskData.stageId,
-    };
-    await this.updateTask(t, updatedTask, taskId);
-    const mappedTaskRepo = taskData.repos?.map((repo) => ({
-      repo_id: Number(repo.repoId),
-      task_branch_name: String(repo.branchName),
-      task_id: Number(taskId),
-    })) || [];
-    await TaskRepoService.handleUpdateTaskRepos(t, mappedTaskRepo, taskId);
-  });
-};
+  async updateTaskTransactionService(taskData: TaskData, taskId: number) {
+    await sequelize.transaction(async (t) => {
+      const updatedTask: TaskUpdateData = {
+        id: taskId,
+        name: taskData.name,
+        description: taskData.description,
+        stage_id: taskData.stageId,
+      };
+      await this.updateTask(t, updatedTask, taskId);
+      const mappedTaskRepo =
+        taskData.repos?.map((repo) => ({
+          repo_id: Number(repo.repoId),
+          task_branch_name: String(repo.branchName),
+          task_id: Number(taskId),
+        })) || [];
+      await TaskRepoService.handleUpdateTaskRepos(t, mappedTaskRepo, taskId);
+      const mappedUserTask: userTaskData[] = taskData.users.map((userTask) => ({
+        user_id: userTask.userId,
+        role_id: userTask.roleId,
+        task_id: taskId,
+      }));
+      await UserTaskService.handleUserTasks(t, mappedUserTask, taskId);
+    });
+  }
 
-async updateTask (
-  t: Transaction,
-  taskData: TaskUpdateData,
-  taskId: number
-){
-  return await Task.update(taskData, {
-    where: {
-      id: taskId,
-    },
-    transaction: t,
-  });
-};
+  async updateTask(t: Transaction, taskData: TaskUpdateData, taskId: number) {
+    return await Task.update(taskData, {
+      where: {
+        id: taskId,
+      },
+      transaction: t,
+    });
+  }
   async deleteTask(id: string) {
     const task = await Task.findByPk(Number(id));
     if (!task) throw new Error("Task Not Found");
