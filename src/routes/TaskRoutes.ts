7c913038f2c8d855cdfda115fa3b2381875edab2
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-10 17:24:54 +0530

    Taruun | Refactored the format

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,12 +1,11 @@
-import  express  from "express";
+import express from "express";
 import TaskController from "../controllers/TaskController";
 const router = express.Router();
 
-router.post('/', TaskController.createTask);
-router.post('/list', TaskController.getAllTask);
-router.get('/:id', TaskController.getTaskById);
-router.put('/:id', TaskController.updateTask);
-router.delete('/:id',TaskController.deleteTask);
-
+router.post("/", TaskController.createTask);
+router.post("/list", TaskController.getAllTask);
+router.get("/:id", TaskController.getTaskById);
+router.put("/:id", TaskController.updateTask);
+router.delete("/:id", TaskController.deleteTask);
 
 export default router;
