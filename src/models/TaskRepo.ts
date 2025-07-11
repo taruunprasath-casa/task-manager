bb1f05114d10942384e7646a28dad35a0d75d51d
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-11 17:51:32 +0530

    Taruun | added alias and changed foreign key for repo

--- src/models/TaskRepo.ts
+++ src/models/TaskRepo.ts
@@ -1,68 +1,72 @@
-    import { CreationOptional, DataTypes, ForeignKey, InferAttributes, Model } from "sequelize";
-    import { sequelize } from "../db/sequelize";
-    import { Task } from "./Task";
-    import { Repo } from "./Repo";
+import {
+  CreationOptional,
+  DataTypes,
+  ForeignKey,
+  InferAttributes,
+  Model,
+} from "sequelize";
+import { sequelize } from "../db/sequelize";
+import { Task } from "./Task";
+import { Repo } from "./Repo";
 
-    export class TaskRepo extends Model<InferAttributes<TaskRepo>>{
-        declare id?: CreationOptional<number>;
-        declare task_id: ForeignKey<Task["id"]>;
-        declare repo_id: ForeignKey<Repo["id"]>;
-        declare task_branch_name: String;
-        declare createdAt?:CreationOptional<Date>;
-        declare updatedAt?:CreationOptional<Date>;
-        
-    }
+export class TaskRepo extends Model<InferAttributes<TaskRepo>> {
+  declare id?: CreationOptional<number>;
+  declare task_id: ForeignKey<Task["id"]>;
+  declare repo_id: ForeignKey<Repo["id"]>;
+  declare task_branch_name: String;
+  declare createdAt?: CreationOptional<Date>;
+  declare updatedAt?: CreationOptional<Date>;
+}
 
-    TaskRepo.init({
-        id:{
-            type:DataTypes.NUMBER,
-            primaryKey:true,
-            autoIncrement:true,
-        },
-        task_branch_name:{
-            type:DataTypes.STRING,
-            allowNull:false,
-        },
-        task_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: Task,
-                key: 'id',
-            },
-        },
-        repo_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: Repo,
-                key: 'id',
-            },
-        },
-        createdAt: {
-        type: DataTypes.DATE(3),
-        defaultValue: sequelize.literal("current_timestamp(3)"),
-        allowNull: false,
-        },
-        updatedAt: {
-        type: DataTypes.DATE(3),
-        defaultValue: sequelize.literal("current_timestamp(3)"),
-        allowNull: false,
-        },
-        
+TaskRepo.init(
+  {
+    id: {
+      type: DataTypes.NUMBER,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    
-        {
-            sequelize,
-            tableName:"task_repo",
-            modelName:"taskrepo",
-            timestamps:true,
-            
-        }
-        
-    );
+    task_branch_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    task_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Task,
+        key: "id",
+      },
+    },
+    repo_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: Repo,
+        key: "id",
+      },
+    },
+    createdAt: {
+      type: DataTypes.DATE(3),
+      defaultValue: sequelize.literal("current_timestamp(3)"),
+      allowNull: false,
+    },
+    updatedAt: {
+      type: DataTypes.DATE(3),
+      defaultValue: sequelize.literal("current_timestamp(3)"),
+      allowNull: false,
+    },
+  },
 
-TaskRepo.belongsTo(Task,{foreignKey:'id'});
-Task.hasMany(TaskRepo,{foreignKey:'repo_id'});
-Repo.hasMany(TaskRepo,{foreignKey:"repo_id"});
-TaskRepo.belongsTo(Repo,{foreignKey:"id"});
+  {
+    sequelize,
+    tableName: "task_repo",
+    modelName: "taskrepo",
+    timestamps: true,
+  }
+);
+
+TaskRepo.belongsTo(Task, { foreignKey: "id" });
+Task.hasMany(TaskRepo, { foreignKey: "repo_id" });
+
+Repo.hasMany(TaskRepo, { foreignKey: "repo_id" });
+TaskRepo.belongsTo(Repo, { foreignKey: "repo_id", as: "repo" });
