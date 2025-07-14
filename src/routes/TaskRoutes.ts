865ac1c00a6bb977ae773faa32f202be8d879b20
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-14 10:33:32 +0530

    Taruun | called the taskCommentRouter in the task API

--- src/routes/TaskRoutes.ts
+++ src/routes/TaskRoutes.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import TaskController from "../controllers/TaskController";
+import taskCommentRouter from "./TaskComments";
+
 const router = express.Router();
 
 router.post("/", TaskController.createTask);
@@ -7,5 +9,5 @@ router.post("/list", TaskController.getAllTask);
 router.get("/:id", TaskController.getTaskById);
 router.put("/:id", TaskController.updateTask);
 router.delete("/:id", TaskController.deleteTask);
-
+router.use("/",taskCommentRouter);
 export default router;
