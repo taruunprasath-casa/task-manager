391f01cff801d3df763ded4bc650dc1f8dd7404d
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-15 10:11:46 +0530

    Taruun | Created CRUD API for taskComments

--- src/services/TaskCommentsService.ts
+++ src/services/TaskCommentsService.ts
@@ -9,6 +9,29 @@ class TaskCommentsService{
             user_id:taskCommentData.userId,
         })
     }
+
+    async getAllTaskComments(){
+        return await TaskComments.findAll();
+    }
+    async updateTaskComments(taskId:number, taskCommentData:TaskCommentData, commentId?: number) {
+        return await TaskComments.update({
+            task_updates: taskCommentData.taskUpdates,
+            user_id: taskCommentData.userId,
+        }, {
+            where: { task_id: taskId, id: commentId },
+            returning: true,
+        });
+    }
+    async getTaskCommentsByTaskId(taskId: number) {
+        return await TaskComments.findAll({
+            where: { task_id: taskId },
+        });
+    }
+    async deleteTaskCommentById(commentId: number) {
+        return await TaskComments.destroy({
+            where: { id: commentId },
+        });
+    }
 }
 
 export default new TaskCommentsService();
