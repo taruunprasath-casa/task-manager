6cdd680ad365cddb86b53afaad00e9854df47456
Taruun Prasath G S <taruunprasath@ajirasoft.com>
2025-07-15 10:10:20 +0530

    Taruun | Created api routes for get, put, delete

--- src/routes/TaskComments.ts
+++ src/routes/TaskComments.ts
@@ -3,5 +3,11 @@ import TaskCommentController from "../controllers/TaskCommentController";
 const router = express.Router();
 
 router.post("/:taskid/comments", TaskCommentController.createTaskComments);
-
+router.get("/all/comments", TaskCommentController.getTaskComments);
+router.get("/:taskid/comments", TaskCommentController.getTaskCommentsByTaskId);
+router.put("/:taskid/comments/:commentid", TaskCommentController.updateTaskCommments);
+router.delete(
+  "/:taskid/comments/:commentid",
+  TaskCommentController.deleteTaskComment
+);
 export default router;
